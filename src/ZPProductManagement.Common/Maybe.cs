1602c9e2215f40c3c2f0d409b98dda71bbba1763
flaviogf <flavio.fernandes6@gmail.com>
2020-06-28 16:17:21 -0300

    feat: adding common and result and maybe types

---
+++ src/ZPProductManagement.Common/Maybe.cs
@@ -0,0 +1,21 @@
+﻿namespace ZPProductManagement.Common
+{
+    public struct Maybe<T>
+    {
+        private Maybe(T value)
+        {
+            Value = value;
+        }
+
+        public bool HasValue => Value != null;
+
+        public bool HasNoValue => !HasValue;
+
+        public T Value { get; }
+
+        public static implicit operator Maybe<T>(T value)
+        {
+            return new Maybe<T>(value);
+        }
+    }
+}
---
+++ src/ZPProductManagement.Common/Result.cs
@@ -0,0 +1,47 @@
+﻿namespace ZPProductManagement.Common
+{
+    public class Result
+    {
+        protected Result(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public bool Failure => !Success;
+
+        public bool Success { get; }
+
+        public string Message { get; }
+
+        public static Result Ok()
+        {
+            return new Result(true, default);
+        }
+
+        public static Result Fail(string message)
+        {
+            return new Result(false, message);
+        }
+
+        public static Result<T> Ok<T>(T value)
+        {
+            return new Result<T>(value, true, default);
+        }
+
+        public static Result<T> Fail<T>(string message)
+        {
+            return new Result<T>(default, false, message);
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        public Result(T value, bool success, string message) : base(success, message)
+        {
+            Value = value;
+        }
+
+        public T Value { get; }
+    }
+}
