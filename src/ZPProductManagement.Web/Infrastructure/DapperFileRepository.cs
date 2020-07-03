cdb1e5ecb3ddd7f72636afd3c5a632d8f9d43218
flaviogf <flavio.fernandes6@gmail.com>
2020-07-03 15:03:24 -0300

    feat: adding dapper file repository

---
+++ src/ZPProductManagement.Web/Infrastructure/DapperFileRepository.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using ZPProductManagement.Application;
+using ZPProductManagement.Common;
+
+namespace ZPProductManagement.Web.Infrastructure
+{
+    public class DapperFileRepository : IFileRepository
+    {
+        private readonly IUnitOfWork _uow;
+
+        private readonly ILogger<DapperFileRepository> _logger;
+
+        public DapperFileRepository(IUnitOfWork uow, ILogger<DapperFileRepository> logger)
+        {
+            _uow = uow;
+            _logger = logger;
+        }
+
+        public async Task<Result> Save(CreatedFile file)
+        {
+            try
+            {
+                var sql = "INSERT INTO [dbo].[Files] ([Id], [Name], [Path], [Extension]) VALUES (@Id, @Name, @Path, @Extension)";
+
+                await _uow.Connection.ExecuteAsync(sql, file, transaction: _uow.Transaction);
+
+                return Result.Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+
+                return Result.Fail("Oops... something has gone wrong");
+            }
+        }
+    }
+}
