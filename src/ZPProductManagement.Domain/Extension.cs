17a5413caf4ab8903c5e16174bc1a14ddd1c1da7
flaviogf <flavio.fernandes6@gmail.com>
2020-07-03 14:45:48 -0300

    feat: adding extension class

---
+++ src/ZPProductManagement.Domain/Extension.cs
@@ -0,0 +1,29 @@
+﻿using ZPProductManagement.Common;
+
+namespace ZPProductManagement.Domain
+{
+    public class Extension
+    {
+        private readonly string _value;
+
+        private Extension(string value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator string(Extension extension)
+        {
+            return extension._value;
+        }
+
+        public static Result<Extension> Of(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return Result.Fail<Extension>($"{nameof(value)} must be informed");
+            }
+
+            return Result.Ok(new Extension(value));
+        }
+    }
+}
