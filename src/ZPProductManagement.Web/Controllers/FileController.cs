d379fbc765ee1b2a4bf8d68547500f6ecf3d9be5
flaviogf <flavio.fernandes6@gmail.com>
2020-07-03 15:03:40 -0300

    feat: adding file controller

---
+++ src/ZPProductManagement.Web/Controllers/FileController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using ZPProductManagement.Application;
+using ZPProductManagement.Web.Infrastructure;
+using ZPProductManagement.Web.ViewModels;
+
+namespace ZPProductManagement.Web.Controllers
+{
+    [Route("[controller]")]
+    public class FileController : Controller
+    {
+        private readonly CreateFileApplication _createFileApplication;
+        private readonly IUnitOfWork _uow;
+        private readonly IConfiguration _configuration;
+
+        public FileController(CreateFileApplication createFileApplication, IUnitOfWork uow, IConfiguration configuration)
+        {
+            _createFileApplication = createFileApplication;
+            _uow = uow;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([FromForm] CreateFileViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["Failure"] = "Choose at least a file";
+
+                return RedirectToAction("Index", "Product");
+            }
+
+            var file = viewModel.File;
+
+            var name = file.FileName;
+
+            var extension = Path.GetExtension(file.FileName);
+
+            var path = $"{Path.GetRandomFileName()}{extension}";
+
+            var createFile = new CreateFile(viewModel.Id, name, path, extension);
+
+            var result = await _createFileApplication.Execute(createFile);
+
+            if (result.Failure)
+            {
+                TempData["Failure"] = result.Message;
+
+                return RedirectToAction("Index", "Product");
+            }
+
+            using var stream = new FileStream(Path.Combine(_configuration["Upload:Directory"], path), FileMode.Create, FileAccess.Write);
+
+            await file.CopyToAsync(stream);
+
+            TempData["Success"] = "File has been created";
+
+            _uow.Commit();
+
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
---
+++ src/ZPProductManagement.Web/ViewModels/CreateFileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZPProductManagement.Web.ViewModels
+{
+    public class CreateFileViewModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public IFormFile File { get; set; }
+    }
+}
