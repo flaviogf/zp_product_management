e64c0cb5c9fe1866914be858dfb0b5de86aff3b8
flaviogf <flavio.fernandes6@gmail.com>
2020-07-03 14:44:42 -0300

    feat: adding identifier class

---
+++ src/ZPProductManagement.Domain/Identifier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ZPProductManagement.Common;
+
+namespace ZPProductManagement.Domain
+{
+    public class Identifier
+    {
+        private readonly Guid _value;
+
+        private Identifier(Guid value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator Guid(Identifier identifier)
+        {
+            return identifier._value;
+        }
+
+        public static Result<Identifier> Of(Guid value)
+        {
+            if (value == Guid.Empty)
+            {
+                return Result.Fail<Identifier>($"{nameof(value)} must not be empty");
+            }
+
+            return Result.Ok(new Identifier(value));
+        }
+    }
+}
