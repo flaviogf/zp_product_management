96d228800d5d3665f30d783420b7a551efce9da7
flaviogf <flavio.fernandes6@gmail.com>
2020-07-03 14:47:21 -0300

    feat: adding created file class

---
+++ src/ZPProductManagement.Application/CreatedFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ZPProductManagement.Domain;
+
+namespace ZPProductManagement.Application
+{
+    public class CreatedFile
+    {
+        private readonly File _file;
+
+        internal CreatedFile(File file)
+        {
+            _file = file;
+        }
+
+        public Guid Id => _file.Id;
+
+        public string Name => _file.Name;
+
+        public string Path => _file.Path;
+
+        public string Extension => _file.Extension;
+    }
+}
