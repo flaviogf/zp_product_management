2cbf6a418ca3c1b2e1c24e05cc57ccce18839845
flaviogf <flavio.fernandes6@gmail.com>
2020-07-03 14:45:01 -0300

    feat: adding name class

---
+++ src/ZPProductManagement.Domain/Name.cs
@@ -0,0 +1,29 @@
+﻿using ZPProductManagement.Common;
+
+namespace ZPProductManagement.Domain
+{
+    public class Name
+    {
+        private readonly string _value;
+
+        private Name(string value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator string(Name name)
+        {
+            return name._value;
+        }
+
+        public static Result<Name> Of(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return Result.Fail<Name>($"{nameof(value)} must be informed");
+            }
+
+            return Result.Ok(new Name(value));
+        }
+    }
+}
