0dfc3a40e75aa7b6d0959751f44e148faa253453
flaviogf <flavio.fernandes6@gmail.com>
2020-07-02 22:07:32 -0300

    feat: adding category

---
+++ ZPProductManagement.Domain/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ZPProductManagement.Domain
+{
+    public class Category
+    {
+        public Category(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Guid Id { get; }
+
+        public string Name { get; }
+    }
+}
