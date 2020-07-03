ae5ef41adf8d11b43f1db9603bca90586da3ce07
flaviogf <flavio.fernandes6@gmail.com>
2020-07-03 14:45:25 -0300

    feat: adding path class

---
+++ src/ZPProductManagement.Domain/Path.cs
@@ -0,0 +1,29 @@
+﻿using ZPProductManagement.Common;
+
+namespace ZPProductManagement.Domain
+{
+    public class Path
+    {
+        private readonly string _value;
+
+        private Path(string value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator string(Path path)
+        {
+            return path._value;
+        }
+
+        public static Result<Path> Of(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return Result.Fail<Path>($"{nameof(value)} must be informed");
+            }
+
+            return Result.Ok(new Path(value));
+        }
+    }
+}
