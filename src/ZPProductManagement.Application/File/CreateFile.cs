4ee976da87cd096ce9441ae59b647cfedbc6c0cb
flaviogf <flavio.fernandes6@gmail.com>
2020-07-02 22:17:28 -0300

    refactor: moving projects to src folder

---
+++ src/ZPProductManagement.Application/File/CreateFile.cs
@@ -0,0 +1,20 @@
+﻿namespace ZPProductManagement.Application.File
+{
+    public class CreateFile
+    {
+        public Output Execute(Input input)
+        {
+            return null;
+        }
+
+        public class Input
+        {
+
+        }
+
+        public class Output
+        {
+
+        }
+    }
+}
