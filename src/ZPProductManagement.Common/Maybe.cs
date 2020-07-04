c416966c9c057890f5489273c60f17f3b1ac7b31
flaviogf <flavio.fernandes6@gmail.com>
2020-07-04 07:44:56 -0300

    feat: adding Maybe

---
+++ src/ZPProductManagement.Common/Maybe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ZPProductManagement.Common
+{
+    public struct Maybe<T>
+    {
+        private readonly T _value;
+
+        public Maybe(T value)
+        {
+            _value = value;
+        }
+
+        public T Value => _value ?? throw new InvalidOperationException();
+
+        public bool HasValue => _value != null;
+
+        public bool HasNoValue => !HasValue;
+
+        public static implicit operator Maybe<T>(T value)
+        {
+            return new Maybe<T>(value);
+        }
+    }
+}
