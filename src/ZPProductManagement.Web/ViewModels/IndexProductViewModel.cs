a8a35d927e284fb2fb0a424d8bb0bec5abf9f8fc
flaviogf <flavio.fernandes6@gmail.com>
2020-07-07 07:25:49 -0300

    feat: adding IndexProductViewModel

---
+++ src/ZPProductManagement.Web/ViewModels/IndexProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ZPProductManagement.Web.ViewModels
+{
+    public class IndexProductViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
