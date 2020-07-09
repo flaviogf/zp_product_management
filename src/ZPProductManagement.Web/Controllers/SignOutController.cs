f49fe26880164938d8a7371bb6f4a806fba5a28d
flaviogf <flavio.fernandes6@gmail.com>
2020-07-09 16:30:11 -0300

    feat: adding sign out functionality

---
+++ src/ZPProductManagement.Web/Controllers/SignOutController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ZPProductManagement.Web.Infrastructure;
+
+namespace ZPProductManagement.Web.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    public class SignOutController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public SignOutController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Destroy()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Create", "SignIn");
+        }
+    }
+}
