2ec9e4412b04b0742452566dc030e6a6efed3823
flaviogf <flavio.fernandes6@gmail.com>
2020-07-09 16:28:13 -0300

    feat: configuring identity

---
+++ src/ZPProductManagement.Web/Infrastructure/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ZPProductManagement.Web.Infrastructure
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
--- src/ZPProductManagement.Web/Startup.cs
+++ src/ZPProductManagement.Web/Startup.cs
@@ -1,6 +1,9 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +29,7 @@ namespace ZPProductManagement.Web
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IDbConnection>((it) => new SqlConnection(_configuration.GetConnectionString("Default")));
+            services.AddScoped<IDbConnection>((it) => new SqlConnection(_configuration.GetConnectionString("Application")));
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<ICategoryRepository, DapperCategoryRepository>();
@@ -40,10 +43,40 @@ namespace ZPProductManagement.Web
                 it.CreateMap<IProductAdapter, IndexProductViewModel>();
             }, Assembly.GetExecutingAssembly());
 
+            services.AddDbContext<ApplicationDbContext>(it => it.UseSqlite(_configuration.GetConnectionString("Identity")));
+
+            services
+                .AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services
+                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie();
+
+            services.Configure<IdentityOptions>(it =>
+            {
+                it.Password.RequireDigit = false;
+                it.Password.RequiredLength = 6;
+                it.Password.RequiredUniqueChars = 1;
+                it.Password.RequireLowercase = false;
+                it.Password.RequireNonAlphanumeric = false;
+                it.Password.RequireUppercase = false;
+
+                it.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(it =>
+            {
+                it.Cookie.HttpOnly = true;
+                it.LoginPath = "/SignIn";
+                it.SlidingExpiration = true;
+            });
+
             services.AddControllersWithViews();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<ApplicationUser> userManager)
         {
             if (env.IsDevelopment())
             {
@@ -54,7 +87,32 @@ namespace ZPProductManagement.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
             app.UseEndpoints(it => it.MapControllers());
+
+            var userName = _configuration.GetValue<string>("User:UserName");
+
+            var userExists = userManager.FindByNameAsync(userName).Result;
+
+            if (userExists != null)
+            {
+                return;
+            }
+
+            var email = _configuration.GetValue<string>("User:Email");
+
+            var password = _configuration.GetValue<string>("User:Password");
+
+            var user = new ApplicationUser
+            {
+                UserName = userName,
+                Email = email
+            };
+
+            userManager.CreateAsync(user, password).Wait();
         }
     }
 }
