041ea4ad46b27470ae2a880a7eaab41a2f8a8235
flaviogf <flavio.fernandes6@gmail.com>
2020-07-09 16:29:53 -0300

    feat: adding sign in functionality

--- src/ZPProductManagement.Web/Controllers/SignInController.cs
+++ src/ZPProductManagement.Web/Controllers/SignInController.cs
@@ -1,14 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ZPProductManagement.Web.Infrastructure;
+using ZPProductManagement.Web.ViewModels;
 
 namespace ZPProductManagement.Web.Controllers
 {
     [Route("[controller]")]
     public class SignInController : Controller
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public SignInController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([FromForm] CreateSignInViewModel viewModel)
+        {
+            var result = await _signInManager.PasswordSignInAsync(viewModel.Email, viewModel.Password, isPersistent: true, lockoutOnFailure: false);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "Wrong email or password");
+
+                return View(viewModel);
+            }
+
+            return RedirectToAction("Index", "Product");
         }
     }
 }
