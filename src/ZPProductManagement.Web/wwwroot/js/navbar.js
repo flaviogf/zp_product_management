a723aeeaee618a33ea2129e385647f99c19aa9fc
flaviogf <flavio.fernandes6@gmail.com>
2020-07-09 16:28:43 -0300

    feat: adding navbar

---
+++ src/ZPProductManagement.Web/wwwroot/js/navbar.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("body").click(function () {
+        if (!$("[data-navbar-sign-out]").hasClass("navbar__sign-out--visible")) {
+            return;
+        }
+
+        $("[data-navbar-sign-out]").removeClass("navbar__sign-out--visible");
+    });
+
+    $("[data-navbar-avatar]").click(function (e) {
+        e.stopPropagation();
+
+        $("[data-navbar-sign-out]").toggleClass("navbar__sign-out--visible");
+    });
+});
