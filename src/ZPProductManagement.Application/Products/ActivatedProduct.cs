201239f5ee7d4ad5c8937689218c78a81e27c2e2
flaviogf <flavio.fernandes6@gmail.com>
2020-07-12 13:03:40 -0300

    feat: adding activated product class

---
+++ src/ZPProductManagement.Application/Products/ActivatedProduct.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using ZPProductManagement.Domain.Entities;
+using ZPProductManagement.Domain.ValueObjects;
+
+namespace ZPProductManagement.Application.Products
+{
+    internal class ActivatedProduct : Product
+    {
+        public ActivatedProduct
+        (
+            Identifier id,
+            Category category,
+            Name name,
+            Description description,
+            Price price,
+            Quantity quantity,
+            IEnumerable<File> files
+        ) : base
+        (
+            id,
+            category,
+            name,
+            description,
+            price,
+            quantity,
+            files
+        )
+        {
+            Status = Activated;
+        }
+    }
+}
