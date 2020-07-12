be13c0a9c3e550f1832038a489513ce6f14f4f83
flaviogf <flavio.fernandes6@gmail.com>
2020-07-12 13:04:00 -0300

    feat: adding archived product class

---
+++ src/ZPProductManagement.Application/Products/ArchivedProduct.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using ZPProductManagement.Domain.Entities;
+using ZPProductManagement.Domain.ValueObjects;
+
+namespace ZPProductManagement.Application.Products
+{
+    internal class ArchivedProduct : Product
+    {
+        public ArchivedProduct
+        (
+            Identifier id,
+            Category category,
+            Name name,
+            Description description,
+            Price price,
+            Quantity quantity,
+            IEnumerable<File> files
+        ) : base
+        (
+            id,
+            category,
+            name,
+            description,
+            price,
+            quantity,
+            files
+        )
+        {
+            Status = Archived;
+        }
+    }
+}
