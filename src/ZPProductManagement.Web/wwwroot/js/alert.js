fc69dd225307b61c07bb5c6de68f5eb721206302
flaviogf <flavio.fernandes6@gmail.com>
2020-07-12 12:57:41 -0300

    feat: adding alert partial view

---
+++ src/ZPProductManagement.Web/wwwroot/js/alert.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('[data-alert-close]').click(function () {
+        $(this).parent().fadeOut(500);
+    });
+
+    setTimeout(function () {
+        $('[data-alert]').fadeOut(500);
+    }, 3500);
+});
