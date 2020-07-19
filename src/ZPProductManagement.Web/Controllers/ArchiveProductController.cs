19aacf7c56ae3fc8f36aa74660ff3d53c4d4b8c7
flaviogf <flavio.fernandes6@gmail.com>
2020-07-19 19:30:21 -0300

    feat: adding ArchiveProductController

---
+++ src/ZPProductManagement.Web/Controllers/ArchiveProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZPProductManagement.Application.Products;
+using ZPProductManagement.Web.Infrastructure;
+
+namespace ZPProductManagement.Web.Controllers
+{
+    [Authorize]
+    [Route("Product/{id}/archive")]
+    public class ArchiveProductController : Controller
+    {
+        private readonly ArchiveProduct _archiveProduct;
+        private readonly IUnitOfWork _uow;
+
+        public ArchiveProductController(ArchiveProduct archiveProduct, IUnitOfWork uow)
+        {
+            _archiveProduct = archiveProduct;
+            _uow = uow;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Guid id)
+        {
+            var result = await _archiveProduct.Execute(id);
+
+            if (result.Failure)
+            {
+                TempData["Failure"] = result.Message;
+
+                return RedirectToAction("Index", "Product");
+            }
+
+            TempData["Success"] = "Product has been archived";
+
+            _uow.Commit();
+
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
