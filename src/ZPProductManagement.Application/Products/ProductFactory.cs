450ccec9030ca307dba34c8ffcaa5c9551ad883a
flaviogf <flavio.fernandes6@gmail.com>
2020-07-19 19:27:33 -0300

    feat: adding ProductFactory

---
+++ src/ZPProductManagement.Application/Products/ProductFactory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using ZPProductManagement.Common.Enums;
+using ZPProductManagement.Domain.Entities;
+using ZPProductManagement.Domain.ValueObjects;
+
+namespace ZPProductManagement.Application.Products
+{
+    public class ProductFactory
+    {
+        public static Product Create(Identifier id, Category category, Name name, Description description, Price price, Quantity quantity, IEnumerable<File> files, EStatus status)
+        {
+            return status switch
+            {
+                EStatus.Activated => new ActivatedProduct(
+                    id,
+                    category,
+                    name,
+                    description,
+                    price,
+                    quantity,
+                    files
+                ),
+                EStatus.Archived => new ArchivedProduct(
+                    id,
+                    category,
+                    name,
+                    description,
+                    price,
+                    quantity,
+                    files
+                ),
+                EStatus.Deleted => new DeletedProduct(
+                    id,
+                    category,
+                    name,
+                    description,
+                    price,
+                    quantity,
+                    files
+                ),
+                _ => new Product
+                (
+                    id,
+                    category,
+                    name,
+                    description,
+                    price,
+                    quantity,
+                    files
+                ),
+            };
+        }
+    }
+}
